c7513b1ebbaf4988d2725a3a3cd15efcce88194e
Murat Demir <murat@muratdemir.gen.tr>
2022-06-22 22:03:05 +0300

    [Murat] Implement mongo client to user store.

--- pkg/store/user.go
+++ pkg/store/user.go
@@ -3,6 +3,7 @@ package store
 import (
 	"context"
 	"github.com/muratdemir0/faceit-task/internal/config"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -11,17 +12,16 @@ type UserStore struct {
 	config *config.Mongo
 }
 type User struct {
-	ID        string `json:"id"`
-	FirstName string `json:"first_name"`
-	LastName  string `json:"last_name"`
-	Nickname  string `json:"nickname"`
-	Password  string `json:"password"`
-	Email     string `json:"email"`
-	Country   string `json:"country"`
+	ID        string `json:"id" bson:"_id"`
+	FirstName string `json:"first_name" bson:"first_name"`
+	LastName  string `json:"last_name" bson:"last_name"`
+	Nickname  string `json:"nickname" bson:"nickname"`
+	Password  string `json:"password" json:"password"`
+	Email     string `json:"email" bson:"email"`
+	Country   string `json:"country" bson:"country"`
 }
 type FindBy struct {
-	Country string `json:"country"`
-	Email   string `json:"email"`
+	Country string `json:"country" bson:"country"`
 }
 
 func NewUserStore(client *mongo.Client, config *config.Mongo) UserStore {
@@ -29,9 +29,64 @@ func NewUserStore(client *mongo.Client, config *config.Mongo) UserStore {
 }
 
 func (s UserStore) Create(ctx context.Context, cu *User) error {
-	_, err := s.client.Database(s.config.Name).Collection(s.config.Collections.Users).InsertOne(ctx, cu)
+	_, err := s.client.
+		Database(s.config.Name).
+		Collection(s.config.Collections.Users).
+		InsertOne(ctx, cu)
+
 	if err != nil {
 		return err
 	}
 	return nil
+}
+func (s UserStore) Update(ctx context.Context, u *User) error {
+	_, err := s.client.
+		Database(s.config.Name).
+		Collection(s.config.Collections.Users).
+		UpdateOne(ctx, u.ID, u)
+
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s UserStore) Delete(ctx context.Context, userID string) error {
+	_, err := s.client.
+		Database(s.config.Name).
+		Collection(s.config.Collections.Users).
+		DeleteOne(ctx, bson.D{{"_id", userID}})
+	if err != nil {
+		return err
+	}
+	return nil
+}
+func (s UserStore) Find(ctx context.Context, userID string) (User, error) {
+	user := User{}
+	err := s.client.
+		Database(s.config.Name).
+		Collection(s.config.Collections.Users).
+		FindOne(ctx, bson.D{{"_id", userID}}).
+		Decode(user)
+	if err != nil {
+		return User{}, err
+	}
+	return user, nil
+}
+func (s UserStore) FindBy(ctx context.Context, criteria FindBy) ([]User, error) {
+	var users []User
+	cursor, err := s.client.
+		Database(s.config.Name).
+		Collection(s.config.Collections.Users).
+		Find(ctx, bson.D{{"country", bson.D{{"$eq", criteria.Country}}}})
+	if err != nil {
+		return nil, err
+	}
+	defer cursor.Close(ctx)
+	cursorErr := cursor.All(ctx, &users)
+	if cursorErr != nil {
+		return nil, cursorErr
+	}
+
+	return users, nil
 }
