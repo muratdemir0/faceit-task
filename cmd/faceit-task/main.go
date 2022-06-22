540cf607ddb6e871d2b0ea80a3762454c425a8fb
Murat Demir <murat@muratdemir.gen.tr>
2022-06-22 22:03:43 +0300

    [Murat] Prepare application dependencies.

--- cmd/faceit-task/main.go
+++ cmd/faceit-task/main.go
@@ -1,9 +1,19 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"github.com/muratdemir0/faceit-task/internal/config"
+	"github.com/muratdemir0/faceit-task/internal/user"
+	"github.com/muratdemir0/faceit-task/pkg/server"
+	"github.com/muratdemir0/faceit-task/pkg/store"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+	"go.uber.org/zap"
 	"os"
+	"os/signal"
+	"syscall"
 )
 
 func main() {
@@ -20,5 +30,41 @@ func run() error {
 		return err
 	}
 	conf.Print()
+
+	logger, err := zap.NewProduction()
+	defer func() {
+		err = logger.Sync()
+		if err != nil {
+			fmt.Println(err)
+		}
+	}()
+
+	mongoURI := fmt.Sprintf("mongodb://%s:%d", conf.Mongo.Host, conf.Mongo.Port)
+	mongoClient, connectErr := mongo.Connect(context.Background(), options.Client().ApplyURI(mongoURI))
+	if connectErr != nil {
+		return connectErr
+	}
+	if err := mongoClient.Ping(context.TODO(), readpref.Primary()); err != nil {
+		return err
+	}
+	fmt.Println("Successfully connected and pinged.")
+
+	userStore := store.NewUserStore(mongoClient, &conf.Mongo)
+	userService := user.NewService(userStore)
+	userHandler := user.NewHandler(userService)
+
+	handlers := []server.Handler{userHandler}
+
+	s := server.New(conf.Server.Port, handlers, logger)
+
+	shutdownChan := make(chan os.Signal, 1)
+	signal.Notify(shutdownChan, os.Interrupt)
+	signal.Notify(shutdownChan, syscall.SIGINT, syscall.SIGTERM)
+
+	go s.Run()
+
+	<-shutdownChan
+
+	s.Stop()
 	return nil
 }
--- go.mod
+++ go.mod
@@ -3,10 +3,12 @@ module github.com/muratdemir0/faceit-task
 go 1.16
 
 require (
-	github.com/gofiber/fiber/v2 v2.34.1 // indirect
-	github.com/golang/mock v1.6.0 // indirect
-	github.com/smartystreets/goconvey v1.7.2 // indirect
+	github.com/gofiber/fiber/v2 v2.34.1
+	github.com/golang/mock v1.6.0
+	github.com/phayes/freeport v0.0.0-20220201140144-74d24b5ae9f5
+	github.com/smartystreets/goconvey v1.7.2
 	github.com/spf13/viper v1.12.0
 	github.com/stretchr/testify v1.7.1
-	go.mongodb.org/mongo-driver v1.9.1 // indirect
+	go.mongodb.org/mongo-driver v1.9.1
+	go.uber.org/zap v1.17.0
 )
